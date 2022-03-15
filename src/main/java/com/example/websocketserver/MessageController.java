4da5c051e852f0cb68d7a8da947ce4b2e5d88a76
Andy Jenkins <ajenkins970@gmail.com>
2022-03-15 12:11:31 +0000

    Remove return type from controller.

--- src/main/java/com/example/websocketserver/MessageController.java
+++ src/main/java/com/example/websocketserver/MessageController.java
@@ -4,15 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
-import java.time.Instant;
-
 @Controller
 @Slf4j
 public class MessageController {
 
     @MessageMapping("/greeting")
-    public String handle(String greeting) {
+    public void handle(String greeting) {
         log.info("Receiving message '{}'", greeting);
-        return "[" + Instant.now().toString() + ": " + greeting;
     }
 }
